93bab2b9bfc0e0736ccec0421d7173b26f0a8b5f
abass-dev <abassthedev@gmail.com>
2023-01-27 17:45:04 +0100

    add requirements.txt

--- py-calculator.py
+++ py-calculator.py
@@ -1,39 +1,44 @@
+from termcolor import colored
+import os
+
 def welcome():
     print('''\n
-              ___😊 Welcome to py-calculator 😊___\n
-                      By Abass Dev With ❤
+😊 Welcome to py-calculator 😊\n
+      By Abass Dev With ❤
         ''')
-
+         
 def calculator():
-    num1 = int(input('First number '))
-    operator = input('Select operator (+, -, *, % ) ')
-
+    num1 = int(input(colored('First number ','green')))
+    operator = input(colored('Select operator (+, -, *, / ) ','yellow'))
+    num2Text = colored('Second number ','green')
+    
     if operator == '+':
-        num2 = int(input('Second number '))
-        print('{} + {} ='.format(num1, num2), num1 + num2)
+        num2 = int(input(num2Text))
+        print('{}: {} + {} ='.format(colored('RESULT', 'red'), num1, num2), num1 + num2)
 
     elif operator == '-':
-          num2 = int(input('Second number '))
-          print('{} - {} ='.format(num1, num2), num1 - num2)
+          num2 = int(input(num2Text))
+          print('{}: {} - {} ='.format(colored('RESULT', 'red'), num1, num2), num1 - num2)
     
     elif operator == '*':
-          num2 = int(input('Second number '))
-          print('{} * {} ='.format(num1, num2), num1 * num2)
+          num2 = int(input(num2Text))
+          print('{}: {} * {} ='.format(colored('RESULT', 'red'), num1, num2), num1 * num2)
 
-    elif operator == '*':
-          num2 = int(input('Second number '))
-          print('{} % {} ='.format(num1, num2), num1 % num2)
+    elif operator == '/':
+          num2 = int(input(num2Text))
+          print('{}: {} / {} ='.format(colored('RESULT', 'red'), num1, num2), num1 / num2)
 
     else:
         print('Invalid operator')
     again()
 
 def again():
-    checkAgain = input('Do you want to continue? (Y/N)')
+    checkAgain = input('\nDo you want to continue? (Y/N)\n')
     if checkAgain == 'Y':
         calculator()
     else:
-        print('See you again.')
-
+        print(colored('See you again 👋\n', 'green'))
+        os._exit(1)
+        
 welcome()
 calculator()
